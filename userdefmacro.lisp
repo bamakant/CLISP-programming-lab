cbb8266ca0771c82c87a906f76e30e903a6aad3e
Bama Kant <bamakantkar@gmail.com>
2017-09-01 23:51:45 +0530

    creating macro and use it

---
+++ userdefmacro.lisp
@@ -0,0 +1,9 @@
+
+	; This program will create a macro called setTo10
+	; which will set numeric value given by user and pront it
+
+	(defmacro setTO10(num))
+	(setq num 15) (print num)  ;setq and defvar both are used for defining 					    ;variables
+	(defvar a 30)
+	(print a)
+	(setTo10 x) ;calling macro setTo10
